070c054aca14f87e7f974ba029ac3a5f428d940b
omg1503 <76679434+omg1503@users.noreply.github.com>
2021-03-11 21:06:33 +0530

    Add files via upload

---
+++ rubber.js
@@ -0,0 +1,26 @@
+class Rubber {
+    constructor(x, y, radius) {
+      var options = {
+          'restitution':0.8,
+          'friction':5,
+          'density':1.0
+      }
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius = radius;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      
+      strokeWeight(4);
+      stroke("black");
+      fill("blue");
+      ellipse(0, 0, this.radius);
+      pop();
+    }
+  };
